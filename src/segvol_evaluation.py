6e6b7db0b3ce655678ea2495bc70c93d9f303a4d
JesseBrouw <jessebrouwers@hotmail.nl>
2024-05-18 13:08:52 +0200

    added job files for snellius

--- src/segvol_evaluation.py
+++ src/segvol_evaluation.py
@@ -13,6 +13,29 @@ import json
 from monai import transforms
 import os
 import torch 
+import pandas as pd
+import random
+import time
+import argparse
+
+def set_parse():
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument("--dataset_code", type = str, default='0002')
+    parser.add_argument("--output_dir", type = str, default='./')
+    parser.add_argument("--prompts", type = list, default=['point', 'text'])
+    parser.add_argument("--seed", type = int, default=42)
+    parser.add_argument("--randrotate", type = bool, default=False)
+    parser.add_argument("--use_zoom", type = bool, default=True)
+    parser.add_argument("--data_dir", type= str, default='./data/datasets')
+    
+    args = parser.parse_args()
+    return args
+
+def seed_all(seed=42):
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.manual_seed(seed)
 
 class UnionDataset(Dataset):
     def __init__(self, concat_dataset, datasets):
@@ -217,9 +240,33 @@ class Evaluator:
             arguments['point_prompt_group'] = [point_prompt, point_prompt_map]
         
         logits_mask = self.model.forward_test(**arguments)
-        print(data_item['label'].shape)
         dice = self.dice_score(logits_mask[0][0], data_item['label'][0][cls_idx], self.device)
-        print(dice.item())
+        print(dice)
+        return dice
+    
+    def run_dataset(self, args):
+        arguments = {
+                'data_dir' : args.data_dir,
+                'dataset_codes' : [args.dataset_code],
+                'randrotate' : args.randrotate
+            }
+        loader = self.get_test_loader(arguments)
+
+        organ_to_idx = {organ:idx for idx, organ in enumerate(self.categories)}
+
+        result_frame = pd.DataFrame(columns=['dataset', 'organ', 'dice', 'randrotate', 'prompts'])
+
+        idx = 0
+        for item in loader:
+            ct, gt = item['image'], item['label']
+            for organ in organ_to_idx.keys():
+                dice = self.inference(ct.squeeze(0), gt.squeeze(0), prompts=args.prompts, use_zoom=args.use_zoom, cls_idx=organ_to_idx[organ])
+                result_frame.loc[idx] = [args.dataset_code, organ, dice, args.randrotate, ''.join(args.prompts)]
+                idx += 1
+        
+        result_frame.to_csv(os.path.join(args.output_dir, f'{args.dataset_code}_{args.seed}_{args.randrotate}.csv'), index=False)
+
+                
 
     
     def experiment_1(self, datasets=['0007', '0023', '0021', '0018', '0020'], prompts=['text', 'bbox'], use_zoom=True):
@@ -365,8 +412,14 @@ class Evaluator:
 
 
 def main():
+    args = set_parse()
+    seed_all(args.seed)
+
+    start = time.time()
     eval = Evaluator()
-    eval.experiment_2()
+    eval.run_dataset(args)
+    
+    print(f'Evaluation on dataset {args.dataset_code} done. Took {time.time()-start} seconds.')
 
 
 if __name__ == '__main__':
